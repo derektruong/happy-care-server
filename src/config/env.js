bbc7179f1182dce0b1c6b7d60bfc6d80dd431afc
derek <minhduc0589@gmail.com>
2021-10-06 18:53:23 +0700

    change env

--- src/config/env.js
+++ src/config/env.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 module.exports = {
     PORT: process.env.PORT || 3000,
-    SECRET: process.env.SECRET_KEY,
-    CONNECTION_STRING: process.env.CONNECTION_MONGO,
+    SECRET: process.env.JWT_SECRET,
+    CONNECTION_STRING: process.env.MONGODB_URL,
 };
