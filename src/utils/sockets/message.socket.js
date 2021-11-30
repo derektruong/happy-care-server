5bd509aec97c565a286915b74bcfe15ce16b41e3
derek <minhduc0589@gmail.com>
2021-11-30 19:06:03 +0700

    change callback ack when socket on

--- src/utils/sockets/message.socket.js
+++ src/utils/sockets/message.socket.js
@@ -9,16 +9,30 @@ class MessageSocket {
   }
 
   async sendMessage(socket) {
-	socket.on('send-message', async (data, callback) => {
-	  try {
-		const { message, roomId, userId } = data;
-		const newMessage = await this.messageService.saveMessage({ messageContent: message, roomId, userId });
-		socket.broadcast.to(roomId).emit('receive-message', { message, user: newMessage.user, time: newMessage.time });
-		callback(generateBasicAck(true, false, 'message was sent successfully'));
-	  } catch (error) {
-		callback(generateBasicAck(false, true, error.message));
-	  }
-	});
+    socket.on('send-message', async (data, callback) => {
+      logger.Info(`on event 'send-message' to roomId: ${roomId} with userId: ${userId}`);
+      try {
+        const { message, roomId, userId } = data;
+        const newMessage = await this.messageService.saveMessage({
+          messageContent: message,
+          roomId,
+          userId,
+        });
+        socket.broadcast
+          .to(roomId)
+          .emit('receive-message', {
+            message,
+            user: newMessage.user,
+            time: newMessage.time,
+          });
+        callback(
+          generateBasicAck(true, false, 'message was sent successfully')
+        );
+		logger.Info(`emit event 'receive-message' to roomId: ${roomId} with userId: ${userId} and time: ${newMessage.time}`);
+      } catch (error) {
+        callback(generateBasicAck(false, true, error.message));
+      }
+    });
   }
 }
 
